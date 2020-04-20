91ab6de969e98782fe8ceb835fde084ef5b0bd47
franciscominajas <franciscominajas@gmail.com>
2020-04-19 22:09:26 -0500

    Icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,65 @@
-$(document).ready(function(){
-    Pace.on("done",function(){
-        $(".pre_loader").css("display","none");
+$(document).ready(function($) {
+    
+    /* Pre Loader Starts */
+    Pace.on("done", function() {
+        $(".pre_loader").css("display", "none");
     });
     
-    $(".hero_title").each(function(i){
-        var $current_element=$(this);
-        setTimeout(function(){
-            $current_element.addClass("reveal");
-        },i*100)
-    });
+    $(".hero_title").each(function(i) {
+            var $current_element = $(this);
+            setTimeout(function() {
+                $current_element.addClass("reveal");
+            }, i * 100);
+        });
+  
     $("#rotating-text").Morphext({
-        // The [in] animation type. Refer to Animate.css for a list of available animations.
-        animation: "bounceIn",
-        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-        separator: ",",
-        // The delay between the changing of each phrase in milliseconds.
-        speed: 3000,
-        complete: function () {
-            // Called after the entrance animation is executed.
-        }
-    });
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    animation: "bounceIn",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    separator: ",",
+    // The delay between the changing of each phrase in milliseconds.
+    speed: 3000,
+    complete: function () {
+        // Called after the entrance animation is executed.
+    }
+});
+
+         /* Owl Carousel Starts */
+     $('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:false,
+    animateOut: 'fadeOut'
+     });
+    /* Owl Carousel Ends */
     
-    $('.owl-carousel').owlCarousel({
-        items:1,
-        loop:true,
-        margin:0,
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:false,
-        animateOut: 'fadeOut'
+    
+    
+   // Reveal text when in viewport
+    
+     $(".text_reveal:not(.in)").each(function(i) {
+        var $current_element = $(this);
+         $current_element.isInViewport(function(status) {
+            if (status === "entered") {
+                setTimeout(function() {
+                    $current_element.addClass("in");
+                }, i * 100);
+            }
+        });
     });
+// Magnific Popup Image Gallery
+    $('.gallery').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+        enabled: true
+        }
+    // other options
+    });
+
+ 
+
 });
